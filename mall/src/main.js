05d699fcb2fe5ae28b99745830f3ade25bf7ea60
Summer <113973444+1975686741@users.noreply.github.com>
2023-02-09 22:34:44 +0800

    vue3商城

---
+++ mall/src/main.js
@@ -0,0 +1,56 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { // 按需加载vant 组件
+    Swipe,
+    SwipeItem,
+    Skeleton,
+    Lazyload
+} from 'vant'
+import 'vant/lib/index.css'
+import './assets/main.css'  // reset 
+import 'lib-flexible/flexible'  // 移动端适配就做好了
+import router from './router' // 启用路由
+
+// - 启用全家桶
+//     router
+//     store 
+// 
+// reset?  准备
+// html  fontSize? rem 自适应 
+const app = createApp(App)
+
+app
+    .use(Swipe)  // 
+    .use(SwipeItem)
+    .use(Skeleton)
+    .use(Lazyload)
+    // .use()
+    .use(router) // 当我们在做SPA 路由接管一切  pages -> 组件 
+
+
+// vue 过滤器
+app.config.globalProperties.$filters = {
+    prefix(url) {
+        if (url && url.startsWith('http')) {
+            return url
+        } else {
+        url = `http://backend-api-01.newbee.ltd${url}`
+            return url
+        }
+    }
+}
+// app.config.globalProperties.$filters = {
+//         prefix(url) {
+//             // console.log(url, '/////') 
+//             if (url && url.startsWith('http')) {
+//                 return url
+//             } else {
+//                 url = `http://backend-api-01.newbee.ltd${url}`
+//                 return url;
+//             }
+//         }
+//     } 
+
+
+app
+    .mount('#app')
---
+++ mall/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <simple-header name="登录" :noback="true"></simple-header>
+    </div>
+</template>
+
+<script setup>
+import SimpleHeader from '@/components/SimpleHeader.vue';
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
